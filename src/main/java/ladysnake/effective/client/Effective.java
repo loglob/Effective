7436b6f4edfa465b3b8f749f40f981d6a2abe264
loglob <23386237+loglob@users.noreply.github.com>
2022-03-02 18:17:47 +0100

    Removed Satin dependency  caused problems with sodium  and I think it did nothing (??)

--- src/main/java/ladysnake/effective/client/Effective.java
+++ src/main/java/ladysnake/effective/client/Effective.java
@@ -1,55 +1,39 @@
 package ladysnake.effective.client;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
+
 import ladysnake.effective.client.data.PlayerCosmeticData;
-import ladysnake.effective.client.particle.*;
+import ladysnake.effective.client.particle.DropletParticle;
+import ladysnake.effective.client.particle.LavaSplashParticle;
+import ladysnake.effective.client.particle.RippleParticle;
+import ladysnake.effective.client.particle.SplashParticle;
+import ladysnake.effective.client.particle.WaterfallCloudParticle;
 import ladysnake.effective.client.render.entity.model.SplashBottomModel;
 import ladysnake.effective.client.render.entity.model.SplashModel;
 import ladysnake.effective.client.world.WaterfallCloudGenerators;
-import ladysnake.satin.api.event.ShaderEffectRenderCallback;
-import ladysnake.satin.api.experimental.ReadableDepthFramebuffer;
-import ladysnake.satin.api.managed.ManagedShaderEffect;
-import ladysnake.satin.api.managed.ShaderEffectManager;
-import ladysnake.satin.api.managed.uniform.SamplerUniform;
-import ladysnake.satin.api.managed.uniform.UniformMat4;
-import ladysnake.satin.api.util.GlMatrices;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.registry.Registry;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.UUID;
 
 @Environment(EnvType.CLIENT)
 public class Effective implements ClientModInitializer {
     public static final String MODID = "effective";
     public static final Logger logger = LogManager.getLogger("Effective");
-
-    // illuminations constants
-//    public static final Gson COSMETICS_GSON = new GsonBuilder().registerTypeAdapter(PlayerCosmeticData.class, new PlayerCosmeticDataParser()).create();
-
-
-    // illuminations cosmetics
-//    private static final String COSMETICS_URL = "https://illuminations.uuid.gg/data";
-//    public static ImmutableMap<String, AuraData> AURAS_DATA;
-//    public static ImmutableMap<String, DefaultParticleType> PETS_DATA;
-//    public static ImmutableMap<String, OverheadData> OVERHEADS_DATA;
 
     // particle types
     public static DefaultParticleType SPLASH;
@@ -71,56 +55,10 @@ public class Effective implements ClientModInitializer {
         return PLAYER_COSMETICS.get(uuid);
     }
 
-    // satin
-    private static final ManagedShaderEffect FOAM_SHADER = ShaderEffectManager.getInstance()
-            .manage(new Identifier("effective", "shaders/post/foam.json"), managedShaderEffect -> {
-                managedShaderEffect.setUniformValue("Viewport", 0, 0, MinecraftClient.getInstance().getWindow().getFramebufferWidth(), MinecraftClient.getInstance().getWindow().getFramebufferHeight());
-                managedShaderEffect.setSamplerUniform("DepthSampler", ((ReadableDepthFramebuffer) MinecraftClient.getInstance().getFramebuffer()).getStillDepthMap());
-            });
-    private static final SamplerUniform transparencyDepthSampler = FOAM_SHADER.findSampler("TransparencyDepthSampler");
-    private static final SamplerUniform entityDepthSampler = FOAM_SHADER.findSampler("EntityDepthSampler");
-    private static final UniformMat4 inverseTransformMatrix = FOAM_SHADER.findUniformMat4("InverseTransformMatrix");
-    private static final Matrix4f MATRIX4F = new Matrix4f();
-
-//    public static void loadPlayerCosmetics() {
-//        // get illuminations player cosmetics
-//        CompletableFuture.supplyAsync(() -> {
-//            try (Reader reader = new InputStreamReader(new URL(COSMETICS_URL).openStream())) {
-//                if (Config.isDebugMode())
-//                    logger.log(Level.INFO, "Retrieving Illuminations cosmetics from the dashboard...");
-//                return COSMETICS_GSON.<Map<UUID, PlayerCosmeticData>>fromJson(reader, COSMETIC_SELECT_TYPE);
-//            } catch (MalformedURLException e) {
-//                if (Config.isDebugMode())
-//                    logger.log(Level.ERROR, "Could not get player cosmetics because of malformed URL: " + e.getMessage());
-//            } catch (IOException e) {
-//                if (Config.isDebugMode())
-//                    logger.log(Level.ERROR, "Could not get player cosmetics because of I/O Error: " + e.getMessage());
-//            }
-//
-//            return null;
-//        }).exceptionally(throwable -> {
-//            if (Config.isDebugMode())
-//                logger.log(Level.ERROR, "Could not get player cosmetics because wtf is happening", throwable);
-//            return null;
-//        }).thenAcceptAsync(playerData -> {
-//            if (playerData != null) {
-//                PLAYER_COSMETICS = playerData;
-//                if (Config.isDebugMode()) logger.log(Level.INFO, "Player cosmetics successfully registered");
-//            } else {
-//                PLAYER_COSMETICS = Collections.emptyMap();
-//                if (Config.isDebugMode())
-//                    logger.log(Level.WARN, "Player cosmetics could not registered, cosmetics will be ignored");
-//            }
-//        }, MinecraftClient.getInstance());
-//    }
-
     @Override
     public void onInitializeClient() {
         // load config
         Config.load();
-
-        // get illuminations player cosmetics
-//        loadPlayerCosmetics();
 
         // register model layers
         EntityModelLayerRegistry.registerModelLayer(SplashModel.MODEL_LAYER, SplashModel::getTexturedModelData);
@@ -145,39 +83,5 @@ public class Effective implements ClientModInitializer {
 
         // sound events
         AMBIENCE_WATERFALL = Registry.register(Registry.SOUND_EVENT, AMBIENCE_WATERFALL.getId(), AMBIENCE_WATERFALL);
-
-        ShaderEffectRenderCallback.EVENT.register(tickDelta -> {
-            Framebuffer translucentFramebuffer = MinecraftClient.getInstance().worldRenderer.getTranslucentFramebuffer();
-            Framebuffer entityFramebuffer = MinecraftClient.getInstance().worldRenderer.getEntityFramebuffer();
-
-            if (translucentFramebuffer != null && entityFramebuffer != null) {
-                transparencyDepthSampler.set(((ReadableDepthFramebuffer) translucentFramebuffer).getStillDepthMap());
-                entityDepthSampler.set(((ReadableDepthFramebuffer) entityFramebuffer).getStillDepthMap());
-
-//                FOAM_SHADER.render(tickDelta);
-            }
-        });
-
-        WorldRenderEvents.LAST.register(context -> {
-            Framebuffer translucentFramebuffer = MinecraftClient.getInstance().worldRenderer.getTranslucentFramebuffer();
-            Framebuffer entityFramebuffer = MinecraftClient.getInstance().worldRenderer.getEntityFramebuffer();
-            if (translucentFramebuffer != null && entityFramebuffer != null) {
-                ((ReadableDepthFramebuffer) translucentFramebuffer).freezeDepthMap();
-                ((ReadableDepthFramebuffer) entityFramebuffer).freezeDepthMap();
-                inverseTransformMatrix.set(GlMatrices.getInverseTransformMatrix(MATRIX4F));
-            }
-        });
-    }
-
-//    private static class PlayerCosmeticDataParser implements JsonDeserializer<PlayerCosmeticData> {
-//        @Override
-//        public PlayerCosmeticData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-//            JsonObject jsonObject = json.getAsJsonObject();
-//            return new PlayerCosmeticData(jsonObject.get("aura")
-//                    , jsonObject.get("color")
-//                    , jsonObject.get("overhead")
-//                    , jsonObject.get("drip")
-//                    , jsonObject.get("pet"));
-//        }
-//    }
+	}
 }
